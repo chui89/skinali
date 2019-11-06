dbb5d47c1399627b60dcf446ad5ad031b57d4848
Viacheslav Churkin <beck2332@gmail.com>
2019-11-06 22:52:20 +0300

    Подключили скрипты

--- js/main.js
+++ js/main.js
@@ -18,7 +18,17 @@ $(window).load(function () {
 
   /* Настройка select */
   $('.select').on('click', function(){
-    $('.select__dropdown').toggleClass('.select__dropdown__open');
+    $('.select__dropdown').toggleClass('select__dropdown__open');
   });
-
+  $('.select__option').on('click', function(){
+    var value = $(this).attr('data-value');
+    $('#select-type').val(value);
+    $('.select_checked').text(value);
+  });
+  $("a[href^='#']").click(function() {
+    var _href = $(this).attr('href');
+    $('html, body').animate({ scrollTop: $(_href).offset().top -100 + 'px'});
+    return false;
+  });
+  $('input[type="tel"]').mask("+7 (999) 999-99-99");
 });
